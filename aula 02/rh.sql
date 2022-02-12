f1f26d183e5cd9e3610f8c166713a91ff1484dc2
Keyla <Keyladias.dk@hotmail.com>
2022-02-11 21:19:35 -0300

    subindo exercicio 11/02

---
+++ aula 02/rh.sql
@@ -0,0 +1,14 @@
+create database db_rh;
+
+use db_rh;
+
+create table tb_funcionarios; (
+ id bigint auto_increment,
+nome varchar(255),
+idade int,
+cargo varchar(255),
+salario decimal,
+primary key(id)
+);
+
+
